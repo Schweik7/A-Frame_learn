4e802d97ccccb0626fe1d82061817b9b199e6a5a
Schweik7 <weibidewo0@gmail.com>
2024-04-02 22:04:44 +0800

    现在是在动画里面发起事件了，而不是在组件的末尾

--- src/components/aframe-text-animation-component.js
+++ src/components/aframe-text-animation-component.js
@@ -40,7 +40,7 @@ AFRAME.registerComponent('text-animation', {
     lineHeight: { type: 'number', default: 1.5 },
     curveSegments: { type: 'number', default: 4 },
     height: { type: 'number', default: 0.05 },
-    signalTarget: { type:'string', default: '' },
+    signalTarget: { type: 'string', default: '' },
   },
   init: function () {
     let data = this.data;
@@ -49,37 +49,19 @@ AFRAME.registerComponent('text-animation', {
     delete this._function;
     const characters = data.text.split('');
     let currentLine = 0;
-    let charIndex = 0;
-    let index = 0;
-    let position = data.position;
-    // console.log("data initial position:", data.position,"data text:", data.text);
+    let charLineIndex = 0; // 在每行中的位置
+    let index = 0; // 在全部文本中的位置
+    let oriPosition = data.position;
+    console.log("text-animation init:", data.text);
     const animateText = () => {
+      if (index >= characters.length) {      
+        // 导出下一个位置，供外部调用，但是由于有动画时间，所以放到动画函数中更好
+        return
+      };
       // data.position = parsePosition(data.position);// 从第二次执行开始，这个data.position可能会从字典变成原始的字符串，导致后续执行错误 
-      if (charIndex >= data.charsPerLine || (currentLine === 0 && charIndex >= data.charsPerLine - data.indent) || index >= characters.length) {
-        currentLine++; // 大于每行换行；首行且小于每行-缩进，则换行；全部打印完毕，则加一行
-        charIndex = 0;
-      }
-      const deltaPosition = {
-        x: (charIndex + (currentLine === 0 ? data.indent : 0)) * (data.fontSize + data.letterSpacing),
-        y: -currentLine * (data.fontSize * data.lineHeight),
-        z: 0
-      };
-      const curPosition = {
-        x: position.x + deltaPosition.x,
-        y: position.y + deltaPosition.y,
-        z: position.z + deltaPosition.z
-      };
-      // 导出下一个位置，供外部调用
-      if (index >= characters.length) { 
-        // window.cur_pos = curPosition; 
-        if (data.signalTarget)
-        {
-          let detail={ nextPosition: curPosition,nextEl:data.signalTarget,originEl:el};
-          console.log("emit signal","detail:",detail);
-          el.emit('textAnimationEnd', detail,true);
-        }
-        return 
-      };
+      let curPosition;
+      ({ curPosition, charIndex: charLineIndex, currentLine } = calcPosition(charLineIndex, data, currentLine, index, characters, oriPosition));
+
 
       const char = characters[index];
       const textEl = document.createElement('a-entity');
@@ -93,15 +75,35 @@ AFRAME.registerComponent('text-animation', {
       });
       textEl.setAttribute('material', { color: data.color, transparent: true, opacity: 0 });
       textEl.setAttribute('position', curPosition);
+      el.appendChild(textEl);
+      charLineIndex++;
+      index++;
       if (functionName && typeof window[functionName] === 'function') {
-        window[functionName](textEl, index, curPosition, data);
+        window[functionName](textEl, index, curPosition, data, charLineIndex, currentLine,oriPosition);
       }
       else { console.log('no function provided,expected ', functionName); }
-      el.appendChild(textEl);
-      charIndex++;
-      index++;
       requestAnimationFrame(animateText);
     };
     animateText();
   }
 });
+
+function calcPosition(charIndex, data, currentLine, index, characters, oriPosition) {
+  if (charIndex >= data.charsPerLine || (currentLine === 0 && charIndex >= data.charsPerLine - data.indent) || index >= characters.length) {
+    currentLine++; // 大于每行换行；首行且小于每行-缩进，则换行；全部打印完毕，则加一行
+    charIndex = 0;
+  }
+  const deltaPosition = {
+    x: (charIndex + (currentLine === 0 ? data.indent : 0)) * (data.fontSize + data.letterSpacing),
+    y: -currentLine * (data.fontSize * data.lineHeight),
+    z: 0
+  };
+  const curPosition = {
+    x: oriPosition.x + deltaPosition.x,
+    y: oriPosition.y + deltaPosition.y,
+    z: oriPosition.z + deltaPosition.z
+  };
+  return { curPosition, charIndex, currentLine };
+}
+window.calcPosition = calcPosition;
+
--- src/index.js
+++ src/index.js
@@ -5,103 +5,102 @@ import './components/aframe-event-set-component';
 import './components/aframe-keyboard-controll-controller';
 // import { v4 as uuid4 } from 'uuid';
 let quez_data = {
-  "quez": {
-    "question": "田田的学习能力不错，但最近发现自己一学习就感到烦躁、无聊和疲惫，没有学习的动力。如果你遇到和田田一样的情况，在面对学习任务时，你最可能有以下哪种行为？",
-    "question_type": "身心倦怠型抑郁",
-    "id": 2
-  },
-  "answers": [
-    {
-      "answer": "寻找一些有趣的学习资料，认真完成学习任务",
-      "id": 6,
-      "score": 0,
-      "quez_id": 2
+    "quez": {
+        "question": "田田的学习能力不错，但最近发现自己一学习就感到烦躁、无聊和疲惫，没有学习的动力。如果你遇到和田田一样的情况，在面对学习任务时，你最可能有以下哪种行为？",
+        "question_type": "身心倦怠型抑郁",
+        "id": 2
     },
-    {
-      "answer": "学习注意力不够集中，但尽力完成学习任务",
-      "id": 7,
-      "score": 1,
-      "quez_id": 2
-    }
-  ]
+    "answers": [
+        {
+            "answer": "寻找一些有趣的学习资料，认真完成学习任务",
+            "id": 6,
+            "score": 0,
+            "quez_id": 2
+        },
+        {
+            "answer": "学习注意力不够集中，但尽力完成学习任务",
+            "id": 7,
+            "score": 1,
+            "quez_id": 2
+        }
+    ]
 }
 
-function textAnimation(textEl, index, position, data) {
-  const baseDelay = 500; // 基础延迟
-  setTimeout(() => {
-    textEl.setAttribute('animation', {
-      property: 'material.opacity',
-      to: 1,
-      dur: 500
-    });
-  }, index * 10 + baseDelay); // 延迟确保逐字显示的效果
+function textAnimation(textEl, index, curPosition, data, charLineIndex, currentLine,oriPosition) {
+    const baseDelay = 500; // 基础延迟
+    setTimeout(() => {
+        textEl.setAttribute('animation', {
+            property: 'material.opacity',
+            to: 1,
+            dur: 500
+        });
+        if (index == data.text.length) {
+            const { curPosition:cPos,charIndex, currentLine:cLine } = calcPosition(charLineIndex, data, currentLine, index, data.text, oriPosition);
+            let nextEl = data.signalTarget || "";
+            let detail={ nextPosition: cPos, nextEl: nextEl };
+            console.log("Emit textAnimationEnd signal detail", detail);
+            textEl.emit("textAnimationEnd",detail ,true);
+        }
+
+    }, index * 4 + baseDelay); // 延迟确保逐字显示的效果
 }
 window.textAnimation = textAnimation;
 
 function generateUniqueId() {
-  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('aframe-scene.html')
-    .then(response => response.text())
-    .then(data => {
-      document.body.innerHTML += data;
-      // const box=document.getElementById('interactive-box');box.emit('textAnimationEnd',{},true);
-      // monitorEvents(element, 'myCustomEvent');
-      function createText(data) {
-        const panel = document.getElementById('text-panel');
-        const answersLength = quez_data.answers.length;
-        quez_data.answers.forEach((answer, index) => {
-          const answerText = document.createElement('a-entity');
-          answerText.id = "answer" + index;
-          panel.appendChild(answerText);
+    fetch('aframe-scene.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.innerHTML += data;
+            // const box=document.getElementById('interactive-box');box.emit('textAnimationEnd',{},true);
+            // monitorEvents(element, 'myCustomEvent');
+            function createText(data) {
+                const panel = document.getElementById('text-panel');
+                const answersLength = quez_data.answers.length;
+                quez_data.answers.forEach((answer, index) => {
+                    const answerText = document.createElement('a-entity');
+                    answerText.id = "answer" + index;
+                    panel.appendChild(answerText);
+                });
 
-          answerText.addEventListener('textAnimationEnd', (event) => {
-            console.log("nextEl", nextEl, "this id", this.id);
-            if (answerText.id == nextEl) {
-              let nextEl = "answer" + (index + 1);
-              // 使用计算好的下一个位置，发送信号给将下一个答案的id
+                panel.addEventListener('textAnimationEnd', (event) => {
+                    const { nextPosition, nextEl } = event.detail;
+                    let textEl = document.getElementById(nextEl);
 
+                    if (textEl) {
+                        let parts = nextEl.split("answer");
+                        let index = parseInt(parts[1], 10) + 1;
+                        let text = quez_data.answers[index - 1].answer;
+                        let nextTarget = "";
+                        if (index < answersLength) nextTarget = "answer" + index;
+                        textEl.setAttribute('text-animation', { text: text, color: '#F00', font: '#myFont', charsPerLine: 15, indent: 0, _function: 'textAnimation', signalTarget: nextTarget, 'position': nextPosition });
+                    }
+                    else return
+                });
+
+                const questionText = document.createElement('a-entity');
+                questionText.setAttribute('text-animation', { text: quez_data.quez.question_type + ': ' + quez_data.quez.question, charsPerLine: 15, color: '#FFF', font: '#myFont', _function: 'textAnimation', signalTarget: "answer0" });// 当问题展示完毕后，发送信号给第一个答案
+                panel.appendChild(questionText);
             }
-          });
-        });
-        panel.addEventListener('textAnimationEnd', (event) => {
-          const { nextPosition, nextEl, origin } = event.detail;
-          let textEl = document.getElementById(nextEl);
-
-          if (textEl) {
-            let parts = nextEl.split("answer");
-            let index = parseInt(parts[1], 10) + 1;
-            let text=quez_data.answers[index-1].answer;
-            console.log("text", text);
-            let nextTarget="";
-            if (index < answersLength)  nextTarget = "answer" + index;
-            textEl.setAttribute('text-animation', { text: text, color: '#F00', font: '#myFont',charsPerLine:15,indent:0, _function: 'textAnimation', signalTarget: nextTarget, 'position': nextPosition });
-          }
-          else return
-        });
-
-        const questionText = document.createElement('a-entity');
-        questionText.setAttribute('text-animation', { text: quez_data.quez.question_type + ': ' + quez_data.quez.question,charsPerLine:15, color: '#FFF', font: '#myFont', _function: 'textAnimation', signalTarget: "answer0" });// 当问题展示完毕后，发送信号给第一个答案
-        panel.appendChild(questionText);
-      }
-      createText(data);
-    }
-    )
-    .catch(error => console.error('Error: ', error));
+            createText(data);
+        }
+        )
+        .catch(error => console.error('Error: ', error));
 });
 let backendhost = 'http://localhost:8000';
 let backendUrl = `${backendhost}/quez/`;
 
 async function fetchData() {
-  try {
-    const response = await fetch(backendUrl);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Failed to fetch data:', error);
-  }
+    try {
+        const response = await fetch(backendUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Failed to fetch data:', error);
+    }
 }
 
 
